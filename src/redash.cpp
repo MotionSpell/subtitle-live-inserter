d73cab7a728c2659757479afdbb9a5a83e80b47d
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2019-12-13 15:49:23 +0100

    xml parsing and transformation

--- src/redash.cpp
+++ src/redash.cpp
@@ -1,19 +1,48 @@
 #include "lib_media/common/metadata_file.hpp"
 #include "lib_media/common/file_puller.hpp"
-#include "lib_media/common/mpeg_dash_parser.hpp"
+#include "lib_media/common/sax_xml_parser.hpp"
+#include "lib_media/common/xml.hpp"
 #include "lib_modules/utils/helper.hpp"
 #include "lib_modules/utils/factory.hpp"
+#include "lib_utils/log_sink.hpp" //enforce
 #include "lib_utils/tools.hpp" //enforce
 #include <thread>
 #include <chrono>
+#include <cassert>
 
 using namespace Modules;
 using namespace Modules::In;
 
 extern const uint64_t g_segmentDurationInMs;
 std::unique_ptr<IFilePuller> createHttpSource();
+int64_t parseIso8601Period(std::string input);
 
 namespace {
+
+Tag parseXml(span<const char> text) {
+	Tag root;
+	std::vector<Tag*> tagStack = { &root };
+
+    //FIXME: our parser uses a map that reorder the attributes 
+	auto onNodeStart = [&](std::string name, std::map<std::string, std::string>& attr) {
+		Tag tag { name };
+
+        for (auto &a : attr)
+			tag.attr.push_back({a.first, a.second});
+
+		tagStack.back()->add(tag);
+		tagStack.push_back(&tagStack.back()->children.back());
+	};
+
+    auto onNodeEnd = [&](std::string) {
+		tagStack.pop_back();
+    };
+
+	saxParse(text, onNodeStart, onNodeEnd);
+
+	assert(tagStack.front()->children.size() == 1);
+	return tagStack.front()->children[0];
+}
 
 class ReDash : public Module {
 	public:
@@ -29,49 +58,83 @@ class ReDash : public Module {
 			output->setMetadata(meta);
 	        m_host->activate(true);
 
-            auto mpdAsText = download(httpSrc.get(), url);
-            if(mpdAsText.empty())
-                throw std::runtime_error("can't get mpd");
-
-            auto mpd = parseMpd({(const char*)mpdAsText.data(), mpdAsText.size()});
-            minUpdatePeriodInMs = mpd->minUpdatePeriod;
+			refreshDashSession(url);
 		}
 
 		void process() override {
-            std::this_thread::sleep_for(std::chrono::milliseconds(minUpdatePeriodInMs));
+			auto mpd = refreshDashSession(url);
 
-            auto mpdAsText = download(httpSrc.get(), url.c_str());
-            if(mpdAsText.empty())
-                throw std::runtime_error("can't get mpd");
+			//TODO: there is no default comparison operator in C++<20
+			//if (mpd == lastMpd)
+			//    return;
 
-            auto mpd = parseMpd({(const char*)mpdAsText.data(), mpdAsText.size()});
+			//add BaseURL
+			std::string baseUrl = url;
+			auto i = baseUrl.rfind('/');
+			if (i != baseUrl.npos)
+				baseUrl = baseUrl.substr(0, i);
+			addBaseUrl(mpd, baseUrl);
 
-            //TODO: there is no default comparison operator in C++<20
-            //if (*mpd == lastMpd)
-            //    return;
+			//ass subtitles
+			addSubtitleAdaptationSet(mpd);
 
-            //add BaseURL
-            for (auto &set : mpd->sets) {
+			auto newMpd = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" + serializeXml(mpd);
 
-            }
+			if (0) {
+				m_host->log(Warning, newMpd.c_str());
+				exit(0);
+			}
 
-            //addSubtitleAdaptationSet(lastMpd);
-            /*    <AdaptationSet id="2XXX" lang="de" segmentAlignment="true">
-                    <Accessibility schemeIdUri="urn:tva:metadata:cs:AudioPurposeCS:2007" value="2" />
-                    <Role schemeIdUri="urn:mpeg:dash:role:2011" value="main" />
-                    <SegmentTemplate timescale="10000000" duration="20000000" startNumber="1XXX" initialization="00c41ecc51a61445875ef0e323d473a8_2_$RepresentationID$-init.mp4XXX" media="00c41ecc51a61445875ef0e323d473a8_2_$RepresentationID$-$Number$XXX.mp4" />
-                    <Representation id="subtitle_07" mimeType="application/mp4" codecs="stpp" bandwidth="9600" startWithSAP="1" />
-                    </AdaptationSet>
-            */
+			std::this_thread::sleep_for(std::chrono::seconds(minUpdatePeriodInSec));
 		}
 
 	private:
+		Tag refreshDashSession(std::string url) {
+			auto mpdAsText = download(httpSrc.get(), url.c_str());
+			if (mpdAsText.empty())
+				throw std::runtime_error("can't get mpd");
+
+			auto mpd = parseXml({ (const char*)mpdAsText.data(), mpdAsText.size() });
+			assert(mpd.name == "MPD");
+
+			assert(!mpd["minimumUpdatePeriod"].empty());
+			minUpdatePeriodInSec = parseIso8601Period(mpd["minimumUpdatePeriod"]);
+
+			return mpd;
+		}
+
+        void addBaseUrl(Tag &mpd, std::string baseUrl) const {
+            for (auto &e : mpd.children)
+                if (e.name == "AdaptationSet") {
+                    Tag tag { "BaseURL" };
+                    tag.attr.push_back({"serviceLocation", baseUrl});
+                    e.add(tag);
+                } else
+                    addBaseUrl(e, baseUrl);
+        }
+
+		void addSubtitleAdaptationSet(Tag& mpd) {
+			for (auto& e : mpd.children)
+				if (e.name == "Period") {
+					auto as = R"|(
+    <AdaptationSet id="2XXX" lang="de" segmentAlignment="true">
+        <Accessibility schemeIdUri="urn:tva:metadata:cs:AudioPurposeCS:2007" value="2" />
+        <Role schemeIdUri="urn:mpeg:dash:role:2011" value="main" />
+        <SegmentTemplate timescale="10000000" duration="20000000" startNumber="1XXX" initialization="00c41ecc51a61445875ef0e323d473a8_2_$RepresentationID$-init.mp4XXX" media="00c41ecc51a61445875ef0e323d473a8_2_$RepresentationID$-$Number$XXX.mp4" />
+        <Representation id="subtitle_07" mimeType="application/mp4" codecs="stpp" bandwidth="9600" startWithSAP="1" />
+    </AdaptationSet>)|";
+					e.add(parseXml({ as, strlen(as) }));
+				}
+				else
+					addSubtitleAdaptationSet(e);
+		}
+
 		KHost* const m_host;
 		OutputDefault* output;
 
         std::string url;
         std::unique_ptr<IFilePuller> httpSrc;
-        int minUpdatePeriodInMs = 0;
+        int minUpdatePeriodInSec = 0;
 };
 
 IModule* createObject(KHost* host, void* va) {
