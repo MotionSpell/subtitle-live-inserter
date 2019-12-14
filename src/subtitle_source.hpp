084be0b7758674d897e692ce3f703608fb08bee5
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2019-12-14 21:51:17 +0100

    missing file

---
+++ src/subtitle_source.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <string>
+#include <lib_media/common/utc_start_time.hpp>
+
+struct SubtitleSourceConfig {
+    std::string filename;
+    uint64_t segmentDurationInMs;
+    IUtcStartTimeQuery const *utcStartTime;
+};
